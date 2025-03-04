c6e5323da55487dcd9e081f3266bf25dc0eb0e7e
SamAl404 <SamuelSulek>
2025-03-04 09:48:31 -0500

    Update some file :D

--- workshop-project/Program.cs
+++ workshop-project/Program.cs
@@ -1,4 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
 
+var users = new Users();
+
+public class Users {
+    public int User_id {get;set;}
+    public int NIT {get;set;}
+    public int User_name {get;set;}
+    public string? Phone {get;set;}
+    public string? Email {get;set;}
+    public string? Adress {get;set;}
+}
+
+public class Vehicles{
+    public int Vehicle_id {get;set;}
+    public string? Plate {get;set;}
+    public string? Chassis {get;set;}
+    public string? Color {get;set;}
+    public string? Engine {get;set;}
+    public int? Id_Brand{get;set;}
     
+}
